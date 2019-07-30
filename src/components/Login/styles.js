9e25a2568cbf592f37cedd8edb49b02f5338a17b
dasari-ananya <ananya.dasari@nirman.io>
2019-07-30 13:14:54 +0530

    login page box given border radius

--- src/components/Login/styles.js
+++ src/components/Login/styles.js
@@ -15,6 +15,7 @@ export const useStyles = theme => ({
     boxSizing: "border-box",
     width: 410,
     padding: "40px 20px 30px",
+    borderRadius: 4,
     margin: "45px auto 0",
     boxShadow: "0 1px 1px 0 rgba(0,0,0,0.07), 0 2px 1px -1px rgba(0,0,0,0.07), 0 1px 3px 0 rgba(0,0,0,0.1)",
     "& h3": {
