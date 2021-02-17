56ba1c792aa89c116df37a17a35469bd4bd06888
Vivek205 <ichbinvivek@gmail.com>
2021-02-17 14:27:55 +0530

    performance script

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, lazy, Suspense } from "react";
-import Amplify from "aws-amplify";
+import Amplify from "@aws-amplify/core";
 import { Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 import { connect } from "react-redux";
